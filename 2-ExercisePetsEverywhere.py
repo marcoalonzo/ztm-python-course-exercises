fc75ebb48aa2c84aff7f573f5e3eea7996045486
Marco Alonzo <marcoalonzo21@gmail.com>
2024-02-27 14:41:27 -0600

    Rename cat exercise, Completed Exercise Pets Everywhere (inheritance)

--- 2-ExercisePetsEverywhere.py
+++ 2-ExercisePetsEverywhere.py
@@ -26,13 +26,28 @@ class Sally(Cat):
         return f'{sounds}'
     
 #1 Add nother Cat
+#Answer:
 class Garfield(Cat):
     def sing(self, sounds):
         return f'{sounds}'
 
 #2 Create a list of all of the pets (create 3 cat instances from the above)
+'''Answer1:
 my_cats = []
+cat1 = Simon('Simon', 10)
+cat2 = Sally('Sally', 2)
+cat3 = Garfield('Garfield', 5)
+my_cats.append(cat1)
+my_cats.append(cat2)
+my_cats.append(cat3)
+'''
+#Answer2 (Simplified):
+my_cats = [Simon('Simon', 10), Sally('Sally', 2), Garfield('Garfield', 5)]
 
 #3 Instantiate the Pet class with all your cats use variable my_pets
+#Answer:
+my_pets = Pets(my_cats)
 
 #4 Output all of the cats walking using the my_pets instance
+#Answer:
+my_pets.walk()
