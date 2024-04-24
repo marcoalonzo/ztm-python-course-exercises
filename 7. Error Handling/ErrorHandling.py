c725bb74dc3666e03dcfb9b80f1e910e308ff4c0
Marco Alonzo <marcoalonzo21@gmail.com>
2024-04-24 13:51:08 -0500

    Finished ErrorHandling.py

--- 7. Error Handling/ErrorHandling.py
+++ 7. Error Handling/ErrorHandling.py
@@ -1,7 +1,22 @@
 #Error Handling
-def hoooohooo():
-    li = [1,2,3]
-    li[5]
 
-hoooohooo()
+while True: #while loop is used to continue asking for an int, if one is not provided
+    try:
+        age = int(input('What is your age? '))
+        10/age
+    except ValueError: #if a value error exists, do the following:
+        print('Please enter a number')
+    '''
+    A second value error will not output since the except block will only run once and
+    as soon as it catches the error it's looking for, it will go back to the while loop
+    '''
+    except ValueError:
+        print('!!!!!')
+    except ZeroDivisionError: #if a 0 div error exists, do the following:
+        print('Please enter age higher than 0')
+    else: #if an integer is provided, print thanks and break the loop
+        print('Thank you!')
+        break
+
+
 
