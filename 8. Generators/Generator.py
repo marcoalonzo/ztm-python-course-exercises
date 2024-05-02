25a3dd57670d06aec99dbb0b816d16a09261685a
Marco Alonzo <marcoalonzo21@gmail.com>
2024-05-02 15:58:57 -0500

    Completed generator.py

--- 8. Generators/Generator.py
+++ 8. Generators/Generator.py
@@ -1,4 +1,14 @@
 #Generator
 
-range(100)
-list(range(100))
+range(100) #creates one by one, not being held in memory
+#list(range(100)) #creates a list of 100 items in memory
+
+def make_list(num):
+    result = []
+    for i in range(num): #not being held in memory
+        result.append(i * 2)
+    return result
+
+my_list = make_list(100) #this list now lives in memory
+print(list(range(1000000)))
+
